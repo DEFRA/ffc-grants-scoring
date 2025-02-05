8b98a61cfd23c9cc08d5dd82ccd4f3cb25443685
David Barker <david.barker@equalexperts.com>
2025-02-05 14:56:08 +0000

    removed erroneous messages from logging
    Signed-off-by: David Barker <david.barker@equalexperts.com>

--- src/api/scoring/handler.js
+++ src/api/scoring/handler.js
@@ -7,38 +7,22 @@ import { log, LogCodes } from '../logging/log.js'
 export const handler = (request, h) => {
   const { answers } = request.payload
   const { grantType } = request.params
-  log(LogCodes.SCORING.REQUEST_RECEIVED, {
-    msg: 'Scoring request received',
-    grantType,
-    answers
-  })
+  log(LogCodes.SCORING.REQUEST_RECEIVED, { grantType, answers })
   const scoringConfig = getScoringConfig(grantType)
 
   if (!scoringConfig) {
-    log(LogCodes.SCORING.CONFIG_MISSING, {
-      msg: 'Grant type not found',
-      grantType
-    })
+    log(LogCodes.SCORING.CONFIG_MISSING, { grantType })
     return h
       .response({ error: 'Invalid grant type' })
       .code(statusCodes.badRequest)
   }
-  log(LogCodes.SCORING.CONFIG_FOUND, {
-    msg: 'Grant type scoring data found',
-    grantType,
-    scoringConfig
-  })
+  log(LogCodes.SCORING.CONFIG_FOUND, { grantType, scoringConfig })
 
   try {
     // Find matching scoring data for the provided questionIds
     const rawScores = score(scoringConfig)(answers)
     const finalResult = mapToFinalResult(scoringConfig, rawScores)
-    log(LogCodes.SCORING.FINAL_RESULT, {
-      msg: 'Scoring final results',
-      grantType,
-      rawScores,
-      finalResult
-    })
+    log(LogCodes.SCORING.FINAL_RESULT, { grantType, rawScores, finalResult })
     return h.response(finalResult).code(statusCodes.ok)
   } catch (error) {
     log(LogCodes.SCORING.CONVERSION_ERROR, { grantType, error })
