49ee9438b5eb78c3f219090734cf7bf04a642278
David Barker <david.barker@equalexperts.com>
2025-03-26 11:42:59 +0000

    Add JSDoc to matrixScore function
    Documented the matrixScore function with JSDoc to clarify its parameters, purpose, and return value. This improves code readability and understanding for future development and maintenance.

--- src/services/scoring/methods/matrix-score.js
+++ src/services/scoring/methods/matrix-score.js
@@ -1,5 +1,13 @@
 import singleScore from '~/src/services/scoring/methods/single-score.js'
 
+/**
+ * Calculates and returns the score for a given set of user answers based on the question scoring configuration
+ * and dependent user answers.
+ * @param {object} questionScoringConfig - The configuration object for scoring, including answers and dependency mappings.
+ * @param {Array} userAnswers - The list of answers provided by the user.
+ * @param {object} dependentUserAnswers - An object containing dependent answers referenced during score calculation.
+ * @returns {{value: number, band: string}} The calculated score object based on the provided answers and scoring configuration.
+ */
 function matrixScore(questionScoringConfig, userAnswers, dependentUserAnswers) {
   const dependentAnswer =
     dependentUserAnswers[questionScoringConfig.scoreDependency][0]
