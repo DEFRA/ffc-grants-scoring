70a7245486274ec3e30ac39201f187940e95f30c
David Barker <david.barker@equalexperts.com>
2025-03-28 11:58:11 +0000

    Refactor matrixScore to improve parameter clarity
    Renamed parameters and variables for better readability and alignment with their purpose. Refactored the logic to enhance code clarity and maintainability without changing functionality.

--- src/services/scoring/methods/matrix-score.js
+++ src/services/scoring/methods/matrix-score.js
@@ -1,25 +1,26 @@
-import singleScore from '~/src/services/scoring/methods/single-score.js'
+import singleScore from './single-score.js'
 
 /**
  * Calculates and returns the score for a given set of user answers based on the question scoring configuration
  * and dependent user answers.
- * @param {object} questionScoringConfig - The configuration object for scoring, including answers and dependency mappings.
+ * @param {object} scoringConfig - The configuration object for scoring, including answers and dependency mappings.
  * @param {Array} userAnswers - The list of answers provided by the user.
  * @param {object} dependentUserAnswers - An object containing dependent answers referenced during score calculation.
  * @returns {{value: number, band: string}} The calculated score object based on the provided answers and scoring configuration.
  */
-function matrixScore(questionScoringConfig, userAnswers, dependentUserAnswers) {
-  const dependentAnswer =
-    dependentUserAnswers[questionScoringConfig.scoreDependency][0]
-  const modifiedScoring = questionScoringConfig.answers.map(
+function matrixScore(scoringConfig, userAnswers, dependentUserAnswers) {
+  const dependencyScoringConfig =
+    dependentUserAnswers[scoringConfig.scoreDependency][0]
+
+  const dependencyModifiedScores = scoringConfig.answers.map(
     ({ answer, score }) => ({
       answer,
-      score: score[dependentAnswer]
+      score: score[dependencyScoringConfig]
     })
   )
 
   return singleScore(
-    { ...questionScoringConfig, answers: [...modifiedScoring] },
+    { ...scoringConfig, answers: dependencyModifiedScores },
     userAnswers
   )
 }
